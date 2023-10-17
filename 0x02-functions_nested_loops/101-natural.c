e3084d260cfce8db8ed149928ca451298f3e4c8d
mellinek <mellinekathomi@gmail.com>
2023-10-17 08:20:19 -0700

    natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,17 +6,18 @@
  *
  * Return: Always 0.
  */
-
-
 int main(void)
 {
-	print_times_table(3);
-	_putchar('\n');
-	print_times_table(5);
-	_putchar('\n');
-	print_times_table(98);
-	_putchar('\n');
-	print_times_table(12);
+
+	int sum = 0;
+
+	for (int i = 1; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("The sum of all multiples of 3 or 5 below 1024 is: %d\n", sum);
 	return (0);
 }
-
