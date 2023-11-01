0d8a0e202639022552cf0dc33996aad280b595ba
mellinek <mellinekathomi@gmail.com>
2023-11-01 12:14:38 -0700

    a

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,18 +1,42 @@
 #include "main.h"
 
-/**
- * malloc_checked - cause normal process termination with a status value of 98
- *
- * @b: allocated memory
- *
- * Return: a pointer to the allocated memory
- */
-void *malloc_checked(unsigned int b)
-{
-	void *i;
+#include <stdlib.h>
 
-	i = malloc(b);
-	if (i == NULL)
-		exist(98);
-	return(i);
+/**
+
+ * create_array - create array of size size and assign char c
+
+ * @size: size of array
+
+ * @c: char to assign
+
+ * Description: creat array of size size and assign char c
+
+ * Return: pointer to array, NULL if fail
+ */
+
+char *create_array(unsigned int size, char c)
+
+{
+
+	char *str;
+
+	unsigned int i;
+
+
+	str = malloc(sizeof(char) * size);
+
+	if (size == 0 || str == NULL)
+
+		return (NULL);
+
+
+
+	for (i = 0; i < size; i++)
+
+		str[i] = c;
+
+	return (str);
+
 }
+
