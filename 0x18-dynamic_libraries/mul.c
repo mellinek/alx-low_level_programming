3d449e7e088255b2e8fb6a11cba2d63b4aa33996
mellinek <mellinekathomi@gmail.com>
2024-01-19 03:14:35 -0800

    dynamic libraries

---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * mul - multiplies two integers
+ * @a:first int
+ * @b: second int
+ * Return: multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * sub - subtracts two integers
+ * @a:first int
+ * @b: second int
+ * Return: subtracted result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
